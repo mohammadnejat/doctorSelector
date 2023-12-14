09d8ad06836e72343300cc472a42c49dade97e76
mohammad <mohammadofahmad@gmail.com>
2023-12-14 13:37:25 +0330

    new look for website

--- pages/[id].js
+++ pages/[id].js
@@ -68,7 +68,7 @@ function Index (res) {
                     ساعات پاسخ گویی به مشاوره تلفنی
                   </span>
                 </div>
-                <div className='grid grid-cols-3 gap-4'>
+                <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
                   {data?.time?.map(items => (
                     <>
                       <div
