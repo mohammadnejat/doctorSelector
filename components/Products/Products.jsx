89626d3ed3c3cef27d44bbb5741b210746336ea0
mohammad <mohammadofahmad@gmail.com>
2023-12-15 15:51:02 +0330

    add new featchers for internet connections

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,15 +1,16 @@
 import MyContext from '@/context/context'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Triangle } from 'react-loader-spinner'
 import ProductArticle from '../ProductArticle/ProductArticle'
 
 function Products () {
   const { usersData, searchedItems } = useContext(MyContext)
+
   return (
     <section>
       <div className='md:mx-auto '>
         <div className='grid grid-cols-1 gap-3 px-2 justify-items-center'>
-          {!usersData.length ? (
+          {usersData.length === 0 ? (
             <div className='p-10 mt-40 bg-white shadow'>
               <Triangle
                 height='150'
@@ -20,6 +21,7 @@ function Products () {
                 wrapperClassName=''
                 visible={true}
               />
+              <h2 className='mt-2 text-xl text-center'>اینترنت را چک کنید</h2>
             </div>
           ) : !searchedItems.length ? (
             usersData.map(item => <ProductArticle {...item} />)
